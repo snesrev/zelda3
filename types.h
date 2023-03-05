d3ab969a0be5e37fe46887d3f0830fb12fb1a14a
Stefan Sperling <stspdotname@users.noreply.github.com>
2023-03-05 23:09:00 +0100

    skip definition of inline swap16() function in case a macro already exists (#175)
    Fixes the build on OpenBSD.

--- types.h
+++ types.h
@@ -63,7 +63,9 @@ static FORCEINLINE uint UintMax(uint a, uint b) { return a > b ? a : b; }
 #define DWORD(x) (*(uint32*)&(x))
 #define XY(x, y) ((y)*64+(x))
 
+#ifndef swap16
 static inline uint16 swap16(uint16 v) { return (v << 8) | (v >> 8); }
+#endif
 
 typedef struct Point16U {
   uint16 x, y;
